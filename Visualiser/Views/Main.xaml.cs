97901d411150bd5c65159a1a005a9906a6d08b27
AdnanHidic <ahidic2@gmail.com>
2014-05-03 15:36:53 +0200

    Moved default view to Views namespace, integrated NuGet and installed OxyPlot.

--- Visualiser/Views/Main.xaml.cs
+++ Visualiser/Views/Main.xaml.cs
@@ -13,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Visualiser
+namespace Visualiser.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Main : Window
     {
-        public MainWindow()
+        public Main()
         {
             InitializeComponent();
         }
